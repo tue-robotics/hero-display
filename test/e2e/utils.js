a0c8cb8fd4feeb5c4bc58e2c26b3e0430b2825fe
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-12-16 20:54:36 +0100

    update test timeout

--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -1,21 +1,23 @@
 import electron from 'electron'
 import { Application } from 'spectron'
 
+const TIMEOUT = 15000
+
 export default {
   afterEach () {
-    this.timeout(10000)
+    this.timeout(TIMEOUT)
 
     if (this.app && this.app.isRunning()) {
       return this.app.stop()
     }
   },
   beforeEach () {
-    this.timeout(10000)
+    this.timeout(TIMEOUT)
     this.app = new Application({
       path: electron,
       args: ['dist/electron/main.js'],
-      startTimeout: 10000,
-      waitTimeout: 10000
+      startTimeout: TIMEOUT,
+      waitTimeout: TIMEOUT
     })
 
     return this.app.start()
