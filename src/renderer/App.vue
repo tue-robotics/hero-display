196d3ffa62e147d5520cdbc011489825a08337a8
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-23 10:47:09 +0100

    Fix battery timeouts

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -151,7 +151,7 @@
       },
       setupClearBatteryType (key, seconds = 10) {
         if (this.batteries[key].TypeTimeOut) {
-          clearTimeout(this.batteries[key].StateTimeOut)
+          clearTimeout(this.batteries[key].TypeTimeOut)
         }
         this.batteries[key].TypeTimeOut = setTimeout(() => {
           this.batteries[key].type = 'dark'
@@ -175,7 +175,7 @@
       },
       handleBatteryMsg (msg) {
         var type = 'info'
-        var percentage = Math.round(msg.percentage * 100)
+        const percentage = Math.round(msg.percentage * 100)
         if (percentage > 40) {
           type = 'success'
         } else if (percentage > 20) {
@@ -183,22 +183,44 @@
         } else {
           type = 'danger'
         }
-        let batteries = this.batteries
-        let key = msg.location
-        batteries[key] = {
-          percentage: percentage,
-          type: type,
-          charging: msg.power_supply_status === 1,
-          TypeTimeOut: null,
-          RemoveTimeOut: null
+
+        const batteries = this.batteries
+        const key = msg.location
+
+        // Get battery or create new one
+        var battery
+        if (!batteries.hasOwnProperty(key)) {
+          battery = {
+            percentage: null,
+            type: null,
+            charging: null,
+            TypeTimeOut: null,
+            RemoveTimeOut: null
+          }
+        } else {
+          battery = batteries[key]
         }
-        this.setupClearBatteryType(key, 10)
-        this.setupRemoveBattery(key, 60)
-        var ordered = {}
+        // Only update the state, not the timeouts, which are done
+        // at the end
+        battery.percentage = percentage
+        battery.type = type
+        battery.charging = msg.power_supply_status === 1 // POWER_SUPPLY_STATUS_CHARGING = 1
+
+        // Update current battery
+        batteries[key] = battery
+
+        // Order batteries, so it shown on alphabetical order
+        const ordered = {}
         Object.keys(batteries).sort().forEach(function (key) {
           ordered[key] = batteries[key]
         })
+
+        // Update batteries with ordered
         this.batteries = ordered
+
+        // Setup Timeouts for this battery
+        this.setupClearBatteryType(key, 10)
+        this.setupRemoveBattery(key, 60)
       }
     },
     mounted () {
