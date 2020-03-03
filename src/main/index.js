66d4f1126cf0b2adca8324f8420b3f3e362d0d24
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-03-03 10:39:23 +0100

    Revert "Add nodeIntegration to work with node 12"
    This reverts commit 77c8062a12cd1cea069b68631dd443f5f315f4c5.

--- src/main/index.js
+++ src/main/index.js
@@ -22,10 +22,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 600,
     useContentSize: true,
-    width: 1024,
-    webPreferences: {
-      nodeIntegration: True
-  }
+    width: 1024
   })
 
   mainWindow.loadURL(winURL)
