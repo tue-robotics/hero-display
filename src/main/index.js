60a3548eacbb67d969e69c943ba0b693be895f36
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-10-23 21:35:26 +0200

    Adapt to eslint 15

--- src/main/index.js
+++ src/main/index.js
@@ -1,19 +1,20 @@
 'use strict'
 
 import { app, BrowserWindow } from 'electron'
+const path = require('path')
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9080'
-  : `file://${__dirname}/index.html`
+  : 'file://' + path.join(__dirname, 'index.html')
 
 function createWindow () {
   /**
