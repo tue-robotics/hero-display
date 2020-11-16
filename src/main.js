4f769df4a5a6a001362db1f44799443064889ca0
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-11-17 00:44:28 +0100

    replace vue-awesome by @fortawesome/vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -4,17 +4,20 @@ import { BContainer, BCol, BRow, BProgress, BProgressBar } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import Icon from 'vue-awesome/components/Icon'
-import 'vue-awesome/icons/bolt'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBolt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import App from './App.vue'
 
-Vue.component('v-icon', Icon)
 Vue.component('b-container', BContainer)
 Vue.component('b-col', BCol)
 Vue.component('b-row', BRow)
 Vue.component('b-progress', BProgress)
 Vue.component('b-progress-bar', BProgressBar)
+
+library.add(faBolt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   render: h => h(App)
